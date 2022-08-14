0da178bf1c72f6716db53a3adb805aab9283a744
Idris <idris.taiwo@me.com>
2022-08-14 01:53:16 +0100

    add husky

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,10 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Button from '@components/Button';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <h1>hello idris</h1>
     </div>
   );
